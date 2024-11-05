4cc8ac1cf1c39ae5d158b63acdef422c9bd592b5
MartiniMichele <martini.michele99@gmail.com>
2024-11-05 10:21:30 +0100

    First commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JsonMockup mk3 = new JsonMockup("a","b","c");
+        mk3.WriteJson();
+    }
+}
